16281d56f3b2250febf085b79e4c6adb698c026e
hari19 <hari19@outlook.in>
2024-05-12 16:33:32 +0530

    rename naive to naive_nat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from mininet.log import setLogLevel, info
-from naive.setup import NetworkTopo
+from naive_nat.setup import NetworkTopo
 from quic_nat.setup import NATNetworkTopo
 from mininet.topo import Topo
 from mininet.net import Mininet
@@ -11,18 +11,16 @@ import time
 import os
 from naive_rl.setup import RLNetworkTopo
 
-def run_naive():
+def run_naive_nat():
     topo = NetworkTopo()
     net = Mininet(topo=topo, link=TCLink)
     net.start()
     net['client'].cmdPrint("ip addr add 10.0.0.45 dev client-eth0")
-    net['r1'].cmdPrint("./venv/bin/python3 ./naive/NAT.py &")
+    net['r1'].cmdPrint("./venv/bin/python3 ./naive_nat/NAT.py &")
     time.sleep(1)
-    # CLI(net)
-    net["server"].cmdPrint("./venv/bin/python ./naive/server.py -c ./ssl/ssl_cert.pem -k ./ssl/ssl_key.pem --port 1000 -v &")
+    net["server"].cmdPrint("./venv/bin/python ./naive_nat/server.py -c ./ssl/ssl_cert.pem -k ./ssl/ssl_key.pem --port 1000 -v &")
     time.sleep(1)
-    net["client"].cmdPrint("./venv/bin/python ./naive/client.py --host 192.168.1.100 --port 1000 -v")
-    # CLI(net)
+    net["client"].cmdPrint("./venv/bin/python ./naive_nat/client.py --host 192.168.1.100 --port 1000 -v")
     net.stop()
 
 
@@ -77,7 +75,7 @@ if __name__ == "__main__":
     
     match c:
         case 1:
-            run_naive()
+            run_naive_nat()
         case 2:
             run_quic_nat()
         case 3:
